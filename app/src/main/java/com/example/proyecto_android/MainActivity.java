5d972c091357c751af87123fcbd50c5c63b4269c
making <miguelmape7@gmail.com>
2021-01-17 18:35:11 +0100

    'change'

--- app/src/main/java/com/example/proyecto_android/MainActivity.java
+++ app/src/main/java/com/example/proyecto_android/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.proyecto_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+
     }
 }
